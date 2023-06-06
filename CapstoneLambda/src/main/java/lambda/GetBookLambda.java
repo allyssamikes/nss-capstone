515b5e3ce1136e39bde4159ff94b9b05d3861e87
Allyssa Mikes <allyssamikes@gmail.com>
2023-06-06 12:01:11 -0400

    updating yaml

--- CapstoneLambda/src/main/java/lambda/GetBookLambda.java
+++ CapstoneLambda/src/main/java/lambda/GetBookLambda.java
@@ -2,16 +2,19 @@ package lambda;
 
 import activity.request.GetBookRequest;
 import activity.result.GetBookResult;
-import org.apache.log4j.LogManager;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.Context;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 public class GetBookLambda extends LambdaActivityRunner<GetBookRequest, GetBookResult>
         implements RequestHandler<LambdaRequest<GetBookRequest>, LambdaResponse> {
 
+    private final Logger log = LogManager.getLogger();
     @Override
     public LambdaResponse handleRequest(LambdaRequest<GetBookRequest> input, Context context) {
+        log.info("GetBookLambda handleRequest");
         return super.runActivity(
                 () -> input.fromPath(path ->
                         GetBookRequest.builder()
